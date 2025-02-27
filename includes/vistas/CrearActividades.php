dea8febb9fbf72c8d17bab887da3143314b57df2
Eva <evsant02@ucm.es>
2025-02-27 17:59:56 +0100

    .

--- includes/vistas/CrearActividades.php
+++ includes/vistas/CrearActividades.php
@@ -7,7 +7,9 @@ $database = "aw";
 
 $conn = new mysqli($servername, $username, $password, $database);
 
-
+if ($conn->connect_error) {
+    die("Error de conexión: " . $conn->connect_error);
+}
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nombre = $_POST['nombre'];
