4cc40fbc58101e2b442d2e56c53e5251dbd6e807
uchaoui <uchaoui@ucm.es>
2025-03-04 14:07:41 +0100

    cambios2

--- p1_g2/index.php
+++ p1_g2/index.php
@@ -8,10 +8,10 @@ $mensaje = $app->getAtributoPeticion('mensaje');
 $contenidoPrincipal = <<<EOS
 <div style="text-align: center; padding: 20px;">
     
-    <img src="ruta/al/logo.png" alt="Logo de la empresa" style="max-width: 200px;">
+    <img src="img/logo.jpg.jpeg" alt="Logo de la organización" style="max-width: 200px;">
 
     
-    <h1>Bienvenido a [Nombre de la Plataforma]</h1>
+    <h1>Bienvenido a Conecta65</h1>
     <p>
         Una plataforma web diseñada para fomentar el envejecimiento activo y combatir la soledad en personas mayores.  
         A través de actividades creativas y colaborativas con propósito social, promovemos la conexión intergeneracional  
