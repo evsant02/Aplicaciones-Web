a40559cc37235b5dab1bf2a7e947069ae56584d9
uchaoui <uchaoui@ucm.es>
2025-03-04 18:12:51 +0100

    ....

--- p1_g2/includes/comun/cabecera.php
+++ p1_g2/includes/comun/cabecera.php
@@ -4,16 +4,16 @@ function mostrarSaludo()
     if (isset($_SESSION["login"]) && ($_SESSION["login"] === true)) 
     {
         echo "Bienvenido, " . $_SESSION['nombre'] . ". <a href='logout.php'>(salir)</a>";
-        echo " <a href='dona.php'>Actividades</a>";
+        /*echo " <a href='dona.php'>Actividades</a>";
         echo " <a href='dona.php'>Dona</a>";
         echo " <a href='nosotros.php'>Qué es Conecta65</a>";
-        echo " <a href='dona.php'>Ayuda</a>";
+        echo " <a href='dona.php'>Ayuda</a>";*/
     } 
     else 
     {
-        echo "Usuario desconocido. <a href='login.php'>Login</a>";
+        /*echo "Usuario desconocido. <a href='login.php'>Login</a>";
         echo " <a href='dona.php'>Dona</a>";
-        echo " <a href='nosotros.php'>Qué es Conecta65</a>";
+        echo " <a href='nosotros.php'>Qué es Conecta65</a>";*/
     }
 }
 ?>
--- p1_g2/includes/login/registerForm.php
+++ p1_g2/includes/login/registerForm.php
@@ -121,4 +121,3 @@ EOF;
 
         return $result;
     }
-}
