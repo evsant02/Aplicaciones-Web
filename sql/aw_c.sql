80210875529ae643a32fced83674f007882c1ed7
javiviGS <javiga29@ucm.es>
2025-03-04 17:21:11 +0100

    Añadir BBDD

--- sql/aw_c.sql
+++ sql/aw_c.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Feb 18, 2025 at 10:50 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 04-03-2025 a las 16:27:47
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,54 +18,168 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `aw_c`
+-- Base de datos: `aw`
 --
-CREATE DATABASE IF NOT EXISTS `aw_c` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `aw_c`;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `usuarios`
+-- Estructura de tabla para la tabla `actividades`
 --
 
-DROP TABLE IF EXISTS `usuarios`;
-CREATE TABLE `usuarios` (
-  `Id` int(11) NOT NULL,
-  `UserName` varchar(50) NOT NULL,
-  `Password` varchar(50) NOT NULL
+CREATE TABLE `actividades` (
+  `id` varchar(5) NOT NULL,
+  `nombre` varchar(20) NOT NULL,
+  `localizacion` varchar(50) NOT NULL,
+  `fecha_hora` datetime(6) NOT NULL,
+  `descripcion` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `usuarios`
+-- Volcado de datos para la tabla `actividades`
 --
 
-INSERT INTO `usuarios` (`Id`, `UserName`, `Password`) VALUES
-(1, 'user', 'userpass'),
-(2, 'admin', 'adminpass'),
-(3, 'user1', 'user1pass'),
-(4, 'user2', 'user2pass'),
-(5, 'user3', 'user3pass');
+INSERT INTO `actividades` (`id`, `nombre`, `localizacion`, `fecha_hora`, `descripcion`) VALUES
+('12345', 'Desayunos solidarios', 'Calle de las vanguardias 20 Primero Derecha', '2025-03-18 08:00:00.000000', 'Actividad de cocina y servicio de desayunos para personas mayores en situación de necesidad.');
+
+-- --------------------------------------------------------
 
 --
--- Indexes for dumped tables
+-- Estructura de tabla para la tabla `actividades-usuario`
+--
+
+CREATE TABLE `actividades-usuario` (
+  `id_usuario` varchar(5) NOT NULL,
+  `id_actividad` varchar(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `actividades-usuario`
+--
+
+INSERT INTO `actividades-usuario` (`id_usuario`, `id_actividad`) VALUES
+('22222', '12345'),
+('33333', '12345');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `donaciones`
+--
+
+CREATE TABLE `donaciones` (
+  `id_donacion` varchar(20) NOT NULL,
+  `id_actividad` varchar(5) NOT NULL,
+  `IBAN` int(24) NOT NULL,
+  `cantidad` int(6) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipos-usuario`
+--
+
+CREATE TABLE `tipos-usuario` (
+  `nombre` varchar(20) NOT NULL,
+  `id_rol` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `tipos-usuario`
+--
+
+INSERT INTO `tipos-usuario` (`nombre`, `id_rol`) VALUES
+('admin', 0),
+('usuario', 1),
+('voluntario', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` varchar(5) NOT NULL,
+  `nombre` varchar(20) NOT NULL,
+  `apellidos` varchar(40) NOT NULL,
+  `fecha_nacimiento` date NOT NULL,
+  `tipo` int(1) NOT NULL,
+  `correo` varchar(40) NOT NULL,
+  `password` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nombre`, `apellidos`, `fecha_nacimiento`, `tipo`, `correo`, `password`) VALUES
+('11111', 'admin1', 'primer administrador', '1995-03-15', 0, 'correoadmin1@gmail.com', 'admin1'),
+('22222', 'usuario1', 'primer usuario', '1945-07-18', 1, 'correousuario1@gmail.com', 'usuario1'),
+('33333', 'voluntario1', 'primer voluntario', '2006-10-19', 2, 'correovoluntario1@gmail.com', 'voluntario1');
+
+--
+-- Índices para tablas volcadas
 --
 
 --
--- Indexes for table `usuarios`
+-- Indices de la tabla `actividades`
+--
+ALTER TABLE `actividades`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `actividades-usuario`
+--
+ALTER TABLE `actividades-usuario`
+  ADD PRIMARY KEY (`id_usuario`,`id_actividad`),
+  ADD KEY `id_actividad` (`id_actividad`);
+
+--
+-- Indices de la tabla `donaciones`
+--
+ALTER TABLE `donaciones`
+  ADD PRIMARY KEY (`id_donacion`) USING BTREE,
+  ADD KEY `id_actividad` (`id_actividad`);
+
+--
+-- Indices de la tabla `tipos-usuario`
+--
+ALTER TABLE `tipos-usuario`
+  ADD PRIMARY KEY (`nombre`),
+  ADD UNIQUE KEY `id_rol` (`id_rol`);
+
+--
+-- Indices de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`Id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `password` (`password`),
+  ADD KEY `rol` (`tipo`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- Restricciones para tablas volcadas
 --
 
 --
--- AUTO_INCREMENT for table `usuarios`
+-- Filtros para la tabla `actividades-usuario`
+--
+ALTER TABLE `actividades-usuario`
+  ADD CONSTRAINT `actividades-usuario_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `actividades-usuario_ibfk_2` FOREIGN KEY (`id_actividad`) REFERENCES `actividades` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `donaciones`
+--
+ALTER TABLE `donaciones`
+  ADD CONSTRAINT `donaciones_ibfk_1` FOREIGN KEY (`id_actividad`) REFERENCES `actividades` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  ADD CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`tipo`) REFERENCES `tipos-usuario` (`id_rol`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
