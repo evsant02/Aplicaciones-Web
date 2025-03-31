9644a33e73f9cd6c0457f9fe61d0a8c6bc41796d
Eva <evsant02@ucm.es>
2025-03-31 13:24:38 +0200

    crearAct

--- p3_g1/index.php
+++ p3_g1/index.php
@@ -16,22 +16,22 @@ if (isset($_SESSION["login"]) && $_SESSION["login"] === true) {
     // Si la sesión está iniciada, muestra la bienvenida sin el botón de inicio de sesión
     $contenidoPrincipal = <<<EOS
     <div class="welcome-container">
+        <p>$mensaje</p>
         <img src="img/logo.jpeg" alt="Logo de la organización" class="logo">
         <h1>Bienvenido a Conecta65</h1>
         <p>Una plataforma web diseñada para fomentar el envejecimiento activo y combatir la soledad en personas mayores.</p> 
         <p>A través de actividades creativas y colaborativas con propósito social, promovemos la conexión intergeneracional y la solidaridad comunitaria.</p>
-        <p>$mensaje</p>
     </div>
     EOS;
 } else {
     // Si la sesión no está iniciada, muestra la bienvenida con el botón de inicio de sesión
     $contenidoPrincipal = <<<EOS
     <div class="welcome-container">
+        <p>$mensaje</p>
         <img src="img/logo.jpeg" alt="Logo de la organización" class="logo">
         <h1>Bienvenido a Conecta65</h1>
         <p>Una plataforma web diseñada para fomentar el envejecimiento activo y combatir la soledad en personas mayores.</p> 
         <p>A través de actividades creativas y colaborativas con propósito social, promovemos la conexión intergeneracional y la solidaridad comunitaria.</p>
-        <p>$mensaje</p>
         <a href="login.php" class="login-button">
             Iniciar Sesión
         </a>
