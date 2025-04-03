71d03ddfc31e078c88b2684d6fd107244003136f
Martina A. <martia01@ucm.es>
2025-04-03 19:48:31 +0200

    estilo y cositas

--- p3_g1/includes/comun/pie.php
+++ p3_g1/includes/comun/pie.php
@@ -1,7 +1,7 @@
 <footer>
     <p>Fomentando el envejecimiento activo y la solidaridad intergeneracional.</p>
     <p>📍 Construyendo comunidad, creatividad y bienestar para nuestros mayores.</p>
-    <p>¿Tienes dudas? Contacta con Nosotras</p>
+    <p>¿Tienes dudas? Contacta con nosotras</p>
     <a href="ayuda.php" class="boton-contacto">
         <button>Contacto</button>
     </a>
--- p3_g1/includes/comun/plantilla.php
+++ p3_g1/includes/comun/plantilla.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<link rel="stylesheet" type="text/css" href="CSS/estilo1.css" />
+		<link rel="stylesheet" type="text/css" href="CSS/estilo.css" />
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">		
 		
