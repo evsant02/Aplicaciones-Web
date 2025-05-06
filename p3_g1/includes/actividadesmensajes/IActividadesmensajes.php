447e77bdde4ee69563932008703e5a082e0607a9
deborarubio <derubio@ucm.es>
2025-05-06 12:22:04 +0200

    borrado carperta act-mens y cambio a sin guion

---
+++ p3_g1/includes/actividadesmensajes/IActividadesmensajes.php
@@ -0,0 +1,25 @@
+<?php
+namespace includes\actividadesmensajes;
+
+/**
+ * Interfaz IActividad
+ * Define los métodos necesarios para la gestión de actividades en el sistema.
+ */
+interface IActividadesmensajes
+{
+    /**
+     * Inserta un nuevo mensaje para un usuario relacionado con una actividad.
+     * @param actividadesmensajesDTO $dto Objeto con los datos del mensaje.
+     * @return bool True si se insertó correctamente, false en caso contrario.
+     */
+    public function insertarMensaje($dto);
+
+    /**
+     * Obtiene todos los mensajes dirigidos a un usuario específico.
+     * @param string $id_usuario ID del usuario.
+     * @return actividadesmensajesDTO[] Array de objetos DTO con los mensajes.
+     */
+    public function obtenerMensajesPorUsuario($id_usuario);
+}
+
+?>
