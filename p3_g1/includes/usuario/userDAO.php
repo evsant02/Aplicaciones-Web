1514e03ee99e184d3ee756039aa434ccb50e8122
Eva <evsant02@ucm.es>
2025-05-14 18:07:31 +0200

    .

--- p3_g1/includes/usuario/userDAO.php
+++ p3_g1/includes/usuario/userDAO.php
@@ -75,12 +75,14 @@ class userDAO extends baseDAO implements IUser
 
             $stmt = $conn->prepare($query);
 
+            $createUserDTO = null;
+
             try {
                 $stmt->bind_param("sssssis", $escId, $escNombre, $escApellidos, $hashedPassword, $escFechaNacimiento, $escTipo, $escCorreo);
 
                 if ($stmt->execute()) {
-                    $idUser = $conn->insert_id;
-                    $createUserDTO=new userDTO($idUser, $escNombre, $escApellidos, $hashedPassword, $escFechaNacimiento, $escTipo, $escCorreo);
+                    //$idUser = $conn->insert_id;
+                    $createUserDTO=new userDTO($escId, $escNombre, $escApellidos, $hashedPassword, $escFechaNacimiento, $escTipo, $escCorreo);
                 }
               
             } finally {
